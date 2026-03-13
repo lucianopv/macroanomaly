d9477a47b4c44bfdb409dd84f9bd12691a21c7db
Luciano Perfetti <lucianopv@gmail.com>
2026-03-13 10:27:34 +0000

    Update test-detect.R

--- tests/testthat/test-detect.R
+++ tests/testthat/test-detect.R
@@ -96,10 +96,10 @@ test_that("Check capa detection", {
 
   expect_s3_class(imf_detect_capa, "maly_detect")
   expect_true(all(c("Country", "TIME_PERIOD", "variable", "capa_strength", "type") %in% colnames(imf_detect_capa)))
-  
+
   # Check that outlier_indicator == 1 when type is point or collective
   expect_true(all((imf_detect_capa$outlier_indicator == 1) == (imf_detect_capa$type %in% c("collective", "point")), na.rm = TRUE))
-  
+
   # Check that point anomalies have capa_strength, collective anomalies don't
   point_rows <- imf_detect_capa$type == "point" & !is.na(imf_detect_capa$type)
   expect_true(all(!is.na(imf_detect_capa$capa_strength[point_rows])))
@@ -117,7 +117,7 @@ test_that("Check multiple methods detection", {
 
   expect_true(all((abs(imf_detect_all$Zscore) > 3) == imf_detect_all$outlier_indicator_zscore))
   expect_true(all((imf_detect_all$outlier_score_isotree > 0.5) == imf_detect_all$outlier_indicator_isotree))
-  
+
   # Check CAPA outlier_indicator matches type being point or collective
   expect_true(all((imf_detect_all$outlier_indicator_capa == 1) == (imf_detect_all$type_capa %in% c("collective", "point")), na.rm = TRUE))
 
@@ -158,4 +158,3 @@ test_that("Check hampel detection", {
   expect_true("outlier_indicator_total" %in% colnames(imf_detect_multi))
 
 })
-EOF 2>&1
